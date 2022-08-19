886bc3f1692a325211f87b2f29ed3a4a82660f38
Hadi A <A_AlSaudi@outlook.com>
2022-08-19 06:52:15 -0700

    Add project files.

---
+++ OpenCamera.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+
+
+os.chdir('C:/Users/hadi/Desktop/Test')
+cap= cv2.VideoCapture(0)
+
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'.mp4'), 30, (width,height))
+
+
+while True:
+    ret,frame= cap.read()
+
+    writer.write(frame)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
+
+
